982e71302f0b6fb4ee12a09a45055288ddad9383
naimur202163 <naimur202163@gmail.com>
2022-06-20 14:10:14 +0600

    PERN

---
+++ db.js
@@ -0,0 +1,16 @@
+const Pool=require("pg").Pool;
+
+
+
+const pool=new Pool({
+        user:"postgres",
+        password:"rony",
+        host:"localhost",
+        port:5432,
+        database:"perntodo"
+});
+
+
+
+module.exports=pool
+
